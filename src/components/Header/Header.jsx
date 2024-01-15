a33ec6c2576e676f852ea4154d9ce1cca57dee67
EXT02D52540 <serhat.bek@consultant.turkcell.com.tr>
2024-01-15 12:05:58 +0300

    styled container

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,11 @@
+import { StyledHeader } from './Header.styled';
+
+const Header = () => {
+  return (
+    <StyledHeader bg='red'>
+      <h1>hubble</h1>
+    </StyledHeader>
+  );
+};
+
+export default Header;
